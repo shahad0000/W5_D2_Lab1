84b54f052a9a48565e00f7cb1ea543bd719c8b96
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-12 13:29:26 +0300

    validate user

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   let postBtn = document.getElementById("post-btn");
   let delBtn = document.getElementById("del-btn");
   let posts = document.getElementById("posts");
+  let form = document.getElementById("post-form");
 
 
-  postBtn.onclick = () => {
+  form.onsubmit = (e) => {
+    e.preventDefault();
     const post = async () => {
       try {
         let res = await fetch(
@@ -18,39 +20,53 @@ document.addEventListener("DOMContentLoaded", () => {
             body: JSON.stringify({
               username: username.value,
               textarea: text.value,
-              img: img.value
+              img: img.value,
             }),
           }
         );
-        let data = await res.json()
-        console.log(data)
+        let data = await res.json();
         let post = document.createElement("div");
         let imgPost = document.createElement("img");
         let userName = document.createElement("div");
         let postContent = document.createElement("div");
         post.classList.add("card", "post");
-       
+
         userName.innerText = data.username;
-        userName.classList.add("post-username")
+        userName.classList.add("post-username");
         post.appendChild(userName);
 
         imgPost.src = data.img;
-        post.appendChild(imgPost)
-        imgPost.classList.add("img-post")
+        post.appendChild(imgPost);
+        imgPost.classList.add("img-post");
         postContent.innerText = data.textarea;
-        postContent.classList.add("post-content")
+        postContent.classList.add("post-content");
 
         post.appendChild(postContent);
 
-        posts.appendChild(post)
-
+        posts.appendChild(post);
       } catch (err) {
         console.log(err);
       }
     };
-    post();
-  };
+    // Validate user
+    if (localStorage.getItem("username") === username.value) {
+      alert("thank you, you already posted");
+    } else {
+      if (username.value.length < 4) {
+        alert("Your username has to be longer than 4 characters");
+      } else {
+        if (text.value.length < 6) {
+          alert(
+            "Please provide a longer text (text has to be longer than 6 characters"
+          );
+        } else {
+          localStorage.setItem("username", username.value);
 
+          post();
+        }
+      }
+    }
+  };
 
   delBtn.onclick = () => {
     const del = async () => {
@@ -62,11 +78,11 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         );
         let post = document.getElementsByClassName("post")[0];
-        posts.remove(post)
+        posts.remove(post);
       } catch (err) {
         console.log(err);
       }
     };
-    del()
+    del();
   };
 });
